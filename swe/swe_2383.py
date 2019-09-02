6adb9bcf4addafd44419c09385a2609d9bc22454
Lee Ah Rum <lar404@naver.com>
2019-09-02 19:52:46 +0900

    점심시간, 원자소멸

--- swe/swe_2383.py
+++ swe/swe_2383.py
@@ -1,54 +1,5 @@
+# 점심 식사시간
+
 import sys
 sys.stdin = open("swe_2383.txt", "r")
 
-# locs_and_dist 배열에 들어있는 정보를 꺼내서 쓴다
-# 사람의 총 명수, 현재 사람의 번호, 계단의 번호
-# 새로 생성할 데이터: 1번계단, 2번계단에 몇 명있는지
-# stair1, stair2 에는 배열을 넘기는데 사람들을 append 해준다
-def recur(M, cur_P, cur_S, cur_T, stair1, stair2):
-    global min_time
-    # 종료 조건
-    if cur_P == (M - 1):
-        # 최소 시간이면 최소 시간 갱신(전역으로)
-        if cur_T < min_time:
-            min_time = cur_T
-            return
-
-    # 넘어온 사람 정보를 가지고 시간을 계산 (계단의 사람 제한 조건 처리)
-    # cur_T를 갱신
-
-
-    # 최소 시간보다 길면 종료
-
-    # 재귀 호출
-    for cur_P in range(M):
-        recur(M, cur_P, 0, cur_T)
-        recur(M, cur_P, 1, cur_T)
-
-
-
-for t in range(1, int(input()) + 1):
-    N = int(input())
-    board = [ list(map(int, input().split())) for _ in range(N)]
-    locs_and_dist = [] # [pc, pr, (distance from stair1, depth of stair1), (distance from stair2, depth of stair2)]
-    stairs = []
-    min_time = 10000000
-
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 0:
-
-                continue
-            elif board[i][j] == 1:
-                locs_and_dist.append([i, j])
-            else:
-                stairs.append((i, j, board[i][j]))
-
-    M = len(locs_and_dist) # number of people
-
-    for i in range(M):
-        D1 = abs(locs_and_dist[i][0] - stairs[0][0]) + abs(locs_and_dist[i][1] - stairs[0][1])
-        D2 = abs(locs_and_dist[i][0] - stairs[1][0]) + abs(locs_and_dist[i][1] - stairs[1][1])
-        locs_and_dist[i] += [(D1, stairs[0][2]), (D2, stairs[1][2])]
-
-    print(f'#{t} {locs_and_dist}')
--- swe/swe_5648.py
+++ swe/swe_5648.py
@@ -1,2 +1,64 @@
 # SWE 5648. 원자 소멸 시뮬레이션
+import sys
+sys.stdin = open("swe_5648.txt", "r")
 
+
+dix = [0, 0, -1, 1]
+diy = [1, -1, 0, 0]
+class Atom_colide:
+    def __init__(self, x, y, location, size):
+        self.x = x
+        self.y = y
+        self.prex = x + dix[location]
+        self.prey = y + diy[location]
+        self.location = location
+        self.size = size
+
+    def move(self):
+        global result
+        x, y, prex, prey, location, size = self.x, self.y, self.prex, self.prey, self.location, self.size
+        new_x, new_y, new_prex, new_prey, new_location, new_size = x + dix[location], y + diy[location], prex + dix[location], prey + diy[location], location, size
+
+        if -1000 <= new_x <= 1000 and -1000 <= new_y <= 1000:
+            if (prex, prey) in atoms and (atoms[(prex, prey)].prex, atoms[(prex, prey)].prey) == (x, y):
+                result += size
+                return
+
+            if (new_x, new_y) not in atom_temp and (new_x, new_y) not in temp:
+                atom_temp[(new_x, new_y)] = Atom_colide(new_x, new_y, new_location, new_size)
+
+            elif (new_x, new_y) in atom_temp:
+                result += (new_size + atom_temp[(new_x, new_y)].size)
+                temp.append((new_x, new_y))
+                del atom_temp[(new_x, new_y)]
+                return
+
+            elif (new_x, new_y) in temp:
+                result += new_size
+                return
+
+for t in range(1, int(input())+1):
+    N = int(input())
+    arr = [ list(map(int, input().split())) for _ in range(N) ]
+
+    atoms = {}
+    atom_temp = {}
+    temp = []
+    result = 0
+
+    for __ in range(len(arr)):
+        x, y, location, size = arr[__][0], arr[__][1], arr[__][2], arr[__][3]
+        atoms[(x, y)] = Atom_colide(x, y, location, size)
+
+    while True:
+        for key, value in atoms.items():
+            value.move()
+        else:
+            if len(atom_temp) == 0:
+                break
+            else:
+                atoms = atom_temp
+                atom_temp = {}
+                temp = []
+
+    print("#{} {}".format(t, result))
